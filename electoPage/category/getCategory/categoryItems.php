71c8a933392217fb4933d2866b39d51dd3359a2b
JackiePhan99 <lazmonbiro@gmail.com>
2023-08-11 09:45:32 +0700

    php & mysql

--- electoPage/category/getCategory/categoryItems.php
+++ electoPage/category/getCategory/categoryItems.php
@@ -87,33 +87,33 @@ if (mysqli_num_rows($catResult) > 0) {
         }
 
         echo '<li class="' . $liClass . '">' .
-            '<div class="category-product-list" >' .
-            '<div class="category-card-product" >' .
-            '<div class="product-type" >' .
-            '<a>' . $row["type"] . '</a>' .
-            '</div >' .
-            '<h5 class="product-name">' .
-            '<a>' . $row["name"] . '</a>' .
-            '</h5>' .
-            '<div class="product-img">' .
-            '<a>' .
-            "<img src=" . $row['image'] . " />" .
-            '</a>' .
-            '</div>' .
-            '<div class="product-buy">' .
-            '<div class="product-price">' .
-            '$' . $row["price"] . '.00' .
-            '</div>' .
-            '<a class="product-buy-icon">' .
-            '<div class="product-add-to-cart">' . '<i class="fa-solid fa-cart-shopping">' . '</i>' . '</div>' .
-            '</a>' .
-            '</div>' .
-            '<div class="card-hidden-items">' .
-            '<a>' . '<i class="fa-solid fa-code-compare">' . '</i>' . 'Compare' . '</a>' .
-            '<a>' . '<i class="fa-regular fa-heart">' . '</i>' . 'Add to Wishlist' . '</a>' .
-            '</div>' .
-            '</div >' .
-            '</div >' .
+                '<div class="category-product-list" >' .
+                    '<div class="category-card-product" >' .
+                        '<div class="product-type" >' .
+                            '<a>' . $row["type"] . '</a>' .
+                        '</div >' .
+                        '<h5 class="product-name">' .
+                            '<a>' . $row["name"] . '</a>' .
+                        '</h5>' .
+                        '<div class="product-img">' .
+                            '<a>' .
+                                "<img src=" . $row['image'] . " />" .
+                            '</a>' .
+                        '</div>' .
+                        '<div class="product-buy">' .
+                            '<div class="product-price">' .
+                                '$' . $row["price"] . '.00' .
+                            '</div>' .
+                            '<a class="product-buy-icon">' .
+                                '<div class="product-add-to-cart">' . '<i class="fa-solid fa-cart-shopping">' . '</i>' . '</div>' .
+                            '</a>' .
+                        '</div>' .
+                        '<div class="card-hidden-items">' .
+                            '<a>' . '<i class="fa-solid fa-code-compare">' . '</i>' . 'Compare' . '</a>' .
+                            '<a>' . '<i class="fa-regular fa-heart">' . '</i>' . 'Add to Wishlist' . '</a>' .
+                        '</div>' .
+                    '</div >' .
+                '</div >' .
             '</li>';
     }
 } else {
